cee370c469dce078df438ceef3a65dcb1fdadc5c
Barry Xu 许庭新 <barry.xu@nio.com>
2019-03-01 17:35:37 +0800

    add re-try logic for run sql

--- db/mysql/mysql.go
+++ db/mysql/mysql.go
@@ -51,6 +51,7 @@ func FetchWithConn(conn *sql.DB, command string, rowHandel func(rowIndex int, ro
 			rows = _rows
 			break
 		}
+		_rows.Close()
 		if err != nil && i == retryTimes-1 {
 			return nil, err
 		}
@@ -98,9 +99,19 @@ func FetchWithConn(conn *sql.DB, command string, rowHandel func(rowIndex int, ro
 // FetchRawWithConn used to query data with a established connection
 func FetchRawWithConn(conn *sql.DB, command string) ([][]sql.RawBytes, error) {
 	// Execute the query
-	rows, err := conn.Query(command)
-	if err != nil {
-		return nil, err
+	var rows *sql.Rows
+	for i := 0; i < retryTimes; i++ {
+		_rows, err := conn.Query(command)
+		if err == nil {
+			rows = _rows
+			break
+		}
+		_rows.Close()
+		if err != nil && i == retryTimes-1 {
+			return nil, err
+		}
+		log.WARNING.Printf("Connect mysql failed, re-conncting %d ...", i+1)
+		time.Sleep(time.Millisecond * 10)
 	}
 	defer rows.Close()
 
